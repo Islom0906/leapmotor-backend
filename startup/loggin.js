612b2affc2621c76e89ac0312e68289f8490d007
Islom0906 <iabdugofurov99@gmail.com>
2023-08-18 15:23:29 +0500

    level error

--- startup/loggin.js
+++ startup/loggin.js
@@ -2,7 +2,7 @@ require('express-async-errors')
 const winston = require("winston");
 module.exports=()=>{
     winston.add(new winston.transports.Console())
-    winston.add(new winston.transports.File({filename:'logs/leapmotor-logs.log',level:'warn'}))
+    winston.add(new winston.transports.File({filename:'logs/leapmotor-logs.log',level:'error'}))
     winston.exceptions.handle(new winston.transports.Console() ,new winston.transports.File({filename:'logs/leapmotor-logs.log'}))
     process.on('unhandledRejection',ex=>{
         throw ex
