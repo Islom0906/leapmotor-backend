1e5dd23c0bd1d8dcf61a28c4a89bb37f4f72635d
Islom0906 <iabdugofurov99@gmail.com>
2023-08-24 17:14:27 +0500

    db base togirlandi

--- startup/db.js
+++ startup/db.js
@@ -4,7 +4,7 @@ const winston=require('winston')
 
 module.exports=()=>{
     // mongodb+srv://iabdugofurov99:ZSC5E90lah3sB2mB@cluster0.xwz5w7b.mongodb.net/
-    mongoose.connect('mongodb+srv://iabdugofurov99:ZSC5E90lah3sB2mB@cluster0.xwz5w7b.mongodb.net/')
+    mongoose.connect('mongodb://localhost:27017/leapmotor')
         .then(()=>{
             winston.debug('MongoDbga ulanish hosil qilindi')
         })
